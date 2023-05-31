d677ed3e3dd309369dff09d418d60ca359b43c48
Ghassan Karwchan <ghassan.karwchan@gmail.com>
2023-05-31 12:13:27 -0600

    add different versions

---
+++ parsefile/AsposeLicense.cs
@@ -0,0 +1,47 @@
+﻿
+using System.Text;
+using Aspose.Pdf;
+
+namespace parsefile;
+
+public class AsposeLicense
+{
+    private static readonly string LicenseString =
+        @"<License>
+              <Data>
+                <LicensedTo>WolfePak Software, LLC</LicensedTo>
+                <EmailTo>licenses@wolfepak.com</EmailTo>
+                <LicenseType>Developer OEM</LicenseType>
+                <LicenseNote>2 Developers And Unlimited Deployment Locations</LicenseNote>
+                <OrderID>211027195925</OrderID>
+                <UserID>826942</UserID>
+                <OEM>This is a redistributable license</OEM>
+                <Products>
+                  <Product>Aspose.PDF for .NET</Product>
+                </Products>
+                <EditionType>Professional</EditionType>
+                <SerialNumber>1566e4e6-d3de-4b92-9c45-928c36275b15</SerialNumber>
+                <SubscriptionExpiry>20221104</SubscriptionExpiry>
+                <LicenseVersion>3.0</LicenseVersion>
+                <LicenseInstructions>https://purchase.aspose.com/policies/use-license</LicenseInstructions>
+              </Data>
+              <Signature>F5Hh6Dr6gkPQcDGrlOaPwIiqsN9z1Gu65PvO4eJx4Qu6Oscfxo/fGvw6kPhDUDuS/DIW9IrFUjRf7yki2erngqKfiaihPrhLKdekCzEc8OUL47BP4WtcHaUs6sPAgqml6/WkyG/F5qzr/3mj1Zg5W5UVdRY4W/4IP3XG0EaQyBM=</Signature>
+            </License>";
+
+    static AsposeLicense()
+    {
+        try
+        {
+            var license = new License();
+            using var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(LicenseString));
+
+            license.SetLicense(memoryStream);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+            
+    }
+}
---
+++ parsefileV21/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using Aspose.Pdf;
+using Aspose.Pdf.Text;
+using parsefileV21;
+
+AsposeLicense license = new AsposeLicense();
+
+Console.WriteLine("start app v 22.10!");
+Document pdfDocument = new Document(@"inputfiles\imageinvoice.pdf");
+var pageCount = pdfDocument.Pages.Count;
+var fulltext = new StringBuilder();
+for (var pageIndex = 1; pageIndex <= pageCount; pageIndex++)
+{
+    var box = pdfDocument.Pages[pageIndex].GetPageRect(true);
+
+    Console.WriteLine("get page number " + pageIndex);
+    var textFragmentAbsorber = new TextFragmentAbsorber()
+    {
+        TextSearchOptions = new TextSearchOptions(false)
+    };
+    pdfDocument.Pages[pageIndex].Accept(textFragmentAbsorber);
+    var textFragmentCollection = textFragmentAbsorber.TextFragments;
+
+    var segments = new List<string>();
+    foreach (var textFragment in textFragmentCollection)
+    {
+        segments.Add(textFragment.Text);
+        fulltext.Append(textFragment.Text).Append(" ");
+    }
+}
+
+// Create a writer and open the file
+TextWriter tw = new StreamWriter(@"inputfiles\parsed-text.txt");
+tw.WriteLine(fulltext);
+// Close the stream
+tw.Close();
+Console.WriteLine("finish the app");
